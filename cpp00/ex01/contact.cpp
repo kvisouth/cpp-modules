2a6bcdca59435e7a29f90f9d0df409381fa62e43
nokosse <visouthivong.kevin@gmail.com>
2023-12-11 13:42:59 +0100

    added comment

--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -1,4 +1,5 @@
 #include "phonebook.hpp"
+#include "contact.hpp"
 
 // Constructor of Contact
 Contact::Contact()
@@ -11,9 +12,34 @@ Contact::~Contact()
 // 'ADD' command
 void Contact::add(void)
 {
-    std::getline(std::cin, firstname);
-    std::getline(std::cin, lastname);
-    std::getline(std::cin, nickname);
-    std::getline(std::cin, phonenumber);
-    std::getline(std::cin, darkestsecret);
+    while (firstname.empty())
+    {
+        std::cout << "First name : ";
+        std::getline(std::cin, firstname);
+    }
+    while (lastname.empty())
+    {
+        std::cout << "Last name : ";
+        std::getline(std::cin, lastname);
+    }
+    while (nickname.empty())
+    {
+        std::cout << "Nickname : ";
+        std::getline(std::cin, nickname);
+    }
+    while (phonenumber.empty())
+    {
+        std::cout << "Phone number : ";
+        std::getline(std::cin, phonenumber);
+    }
+    while (darkestsecret.empty())
+    {
+        std::cout << "Darkest secret : ";
+        std::getline(std::cin, darkestsecret);
+    }
+    std::cout << "First name: " << firstname << std::endl;
+    std::cout << "Last name: " << lastname << std::endl;
+    std::cout << "Nickname: " << nickname << std::endl;
+    std::cout << "Phone number: " << phonenumber << std::endl;
+    std::cout << "Darkest secret: " << darkestsecret << std::endl;
 }
